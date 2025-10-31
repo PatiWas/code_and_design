2d2bfc94f5e36fb9779c65874888ee7103de5d5d
PatiWas <patricia.wassmer@stud.sfgz.ch>
2025-10-31 09:21:16 +0100

    update

---
+++ uebungen/ueb-0303/sketch.js
@@ -0,0 +1,23 @@
+let posX = 0;
+//let posY = 100
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+
+  rect(posX, height/2, 50, 50)
+
+  posX = posX + 1;
+
+  if(posX < 350){
+    
+  }
+
+  //if(posX == windowWidth-50){
+  //  noLoop();
+  //}
+
+}
