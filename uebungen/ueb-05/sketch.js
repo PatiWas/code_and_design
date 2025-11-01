abc768376e6817894a7770b620b63a425fd2b831
PatiWas <patricia.wassmer@stud.sfgz.ch>
2025-11-01 08:48:15 +0100

    Tag 3

---
+++ uebungen/ueb-05/sketch.js
@@ -0,0 +1,19 @@
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+
+
+    // initialisiserung
+  for (let i = 0; i < 10; i++) {
+    // anweisungen
+    ellipse(200 * i, height/2, 200, 200);
+    
+
+  }
+
+}
